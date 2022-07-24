62b82a8b5eb4df532a0b008f915b42fe3c6985aa
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-07-24 19:39:42 +0300

    [ats] Build gem

---
+++ spec/africas/talking/services_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Africas::Talking::Services do
+  it "has a version number" do
+    expect(Africas::Talking::Services::VERSION).not_to be nil
+  end
+end
