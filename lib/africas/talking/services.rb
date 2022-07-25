d8d82c3f79b5d53ba971796aae7a3d9fd048c5e9
sylvance_theone <9350722+Sylvance@users.noreply.github.com>
2022-07-25 07:38:44 +0300

    [ats] Add entities

--- lib/africas/talking/services.rb
+++ lib/africas/talking/services.rb
@@ -19,6 +19,229 @@ module Africas
           Voice.new(app_name: @app_name, app_key: @app_key)
         end
       end
+
+      class VoiceCallEntity
+        attr_accessor :from, :to, :client_request_id
+        def initialize(from:, to:, client_request_id:)
+          @from              = from
+          @to                = to
+          @client_request_id =  client_request_id
+        end
+
+        def to_body
+          {
+            from: @from,
+            to: @to,
+            client_request_id: @client_request_id
+          }
+        end
+      end
+
+      class VoiceSayEntity
+        attr_accessor :text, :voice, :play_beep
+        def initialize(text:, voice:, play_beep:)
+          @text      = text
+          @voice     = voice
+          @play_beep = play_beep
+        end
+
+        def to_body
+          {
+            text: @text,
+            voice: @voice,
+            play_beep: @play_beep
+          }
+        end
+      end
+
+      class VoicePlayEntity
+        attr_accessor :audio_url
+        def initialize(audio_url:)
+          @audio_url = audio_url
+        end
+
+        def to_body
+          {
+            audio_url: @audio_url
+          }
+        end
+      end
+
+      class VoiceGetDigitsEntity
+        attr_accessor :prompt
+        def initialize(prompt:, timed_out_notification:, timeout:, finish_on_key:, callback_url:)
+          @prompt                 = prompt
+          @timed_out_notification = timed_out_notification
+          @timeout                = timeout
+          @finish_on_key          = finish_on_key
+          @callback_url           = callback_url
+        end
+
+        def to_body
+          {
+            prompt: @prompt,
+            timed_out_notification: @timed_out_notification,
+            timeout: @timeout,
+            finish_on_key: @finish_on_key,
+            callback_url: @callback_url
+          }
+        end
+      end
+
+      class VoiceForwardCallEntity
+        attr_accessor :phone_numbers, :ringback_tone, :record, :max_duration, :caller_id, :sequential
+        def initialize(phone_numbers:, ringback_tone:, record:, max_duration:, caller_id:, sequential:)
+          @phone_numbers = phone_numbers
+          @ringback_tone = ringback_tone
+          @record        = record
+          @max_duration  = max_duration
+          @caller_id     = caller_id
+          @sequential    = sequential
+        end
+
+        def to_body
+          {
+            phone_numbers: @phone_numbers,
+            ringback_tone: @ringback_tone,
+            record: @record,
+            max_duration: @max_duration,
+            caller_id: @caller_id,
+            sequential: @sequential
+          }
+        end
+      end
+
+      class VoiceRecordEntity
+        attr_accessor :prompt, :finish_on_key, :maxLength, :trim_silence, :play_beep
+        def initialize(prompt:, finish_on_key:, maxLength:, trim_silence:, play_beep:)
+          @prompt        = prompt
+          @finish_on_key = finish_on_key
+          @maxLength     = maxLength
+          @trim_silence  = trim_silence
+          @play_beep     = play_beep
+        end
+
+        def to_body
+          {
+            prompt: @prompt,
+            finish_on_key: @finish_on_key,
+            maxLength: @maxLength,
+            trim_silence: @trim_silence,
+            play_beep: @play_beep
+          }
+        end
+      end
+
+      class VoiceTerminalRecordEntity
+        attr_accessor :prompt, :play_beep
+        def initialize(prompt:, play_beep:)
+          @prompt    = prompt
+          @play_beep = play_beep
+        end
+
+        def to_body
+          {
+            prompt: @prompt,
+            play_beep: @play_beep
+          }
+        end
+      end
+
+      class VoiceEnqueueEntity
+        attr_accessor :queue_name, :hold_music
+        def initialize(queue_name:, hold_music:)
+          @queue_name = queue_name
+          @hold_music = hold_music
+        end
+
+        def to_body
+          {
+            queue_name: @queue_name,
+            hold_music: @hold_music
+          }
+        end
+      end
+
+      class VoiceDequeueEntity
+        attr_accessor :queue_name, :phone_number
+        def initialize(queue_name:, phone_number:)
+          @queue_name = queue_name
+          @phone_number = phone_number
+        end
+
+        def to_body
+          {
+            queue_name: @queue_name,
+            phone_number: @phone_number
+          }
+        end
+      end
+
+      class VoiceRedirectEntity
+        attr_accessor :handler_url
+        def initialize(handler_url:)
+          @handler_url = handler_url
+        end
+
+        def to_body
+          {
+            handler_url: @handler_url
+          }
+        end
+      end
+
+      class CallbackEntity
+        attr_accessor :callback_response
+        def initialize(callback_response:)
+          @callback_response =  callback_response
+        end
+
+        def is_active
+          response['is_active']
+        end
+
+        def session_id
+          response['session_id']
+        end
+
+        def direction
+          response['direction']
+        end
+
+        def caller_number
+          response['caller_number']
+        end
+
+        def destination_number
+          response['destination_number']
+        end
+
+        def get_digits_response
+          response['get_digits_response']
+        end
+
+        def recording_url
+          response['recording_url']
+        end
+
+        def duration_in_seconds
+          response['duration_in_seconds']
+        end
+
+        def currency_code
+          response['currency_code']
+        end
+
+        def amount
+          response['amount']
+        end
+
+        private
+
+        def response
+          @callback_response
+        end
+      end
     end
   end
 end
--- spec/africas/talking/services_spec.rb
+++ spec/africas/talking/services_spec.rb
@@ -12,46 +12,147 @@ RSpec.describe Africas::Talking::Services do
 
   describe 'Africas::Talking::Services::Voice class' do
     let(:voice) { service.voice }
-    let(:entity) { 'entity' }
 
-    it 'can create a voice call to call AT api' do
-      expect(voice.call(entity: entity)).to eq('Voice call')
+    context 'when doing a voice call api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceCallEntity.new(
+          from: 'from', to: 'to', client_request_id: 'client_request_id'
+        )
+      end
+
+      it 'can create a voice call to call AT api' do
+        expect(voice.call(entity: entity)).to eq('Voice call')
+      end
     end
 
-    it 'can create a voice say to call AT api' do
-      expect(voice.say(entity: entity)).to eq('Voice say')
+    context 'when doing a voice say api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceSayEntity.new(
+          text: 'Something', voice: 'en-GB-Standard-F', play_beep: true
+        )
+      end
+
+      it 'can create a voice say to call AT api' do
+        expect(voice.say(entity: entity)).to eq('Voice say')
+      end
     end
 
-    it 'can create a voice play to call AT api' do
-      expect(voice.play(entity: entity)).to eq('Voice play')
+    context 'when doing a voice play api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoicePlayEntity.new(
+          audio_url: 'http://www.myvoicemailserver.com/audio/vmail.wav'
+        )
+      end
+
+      it 'can create a voice play to call AT api' do
+        expect(voice.play(entity: entity)).to eq('Voice play')
+      end
     end
 
-    it 'can create a voice get_digits call to AT api' do
-      expect(voice.get_digits(entity: entity)).to eq('Voice get_digits')
+    context 'when doing a voice get_digits api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceGetDigitsEntity.new(
+          prompt: 'Enter your account number.',
+          timed_out_notification: 'We failed to get your account number.',
+          timeout: '30',
+          finish_on_key: '#',
+          callback_url: 'http://mycallbackURL.com'
+        )
+      end
+
+      it 'can create a voice get_digits call to AT api' do
+        expect(voice.get_digits(entity: entity)).to eq('Voice get_digits')
+      end
     end
 
-    it 'can create a voice forward_call to call AT api' do
-      expect(voice.forward_call(entity: entity)).to eq('Voice forward_call')
+    context 'when doing a voice forward_call api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceForwardCallEntity.new(
+          phone_numbers: '+254711XXXYYY,+25631XYYZZZZ,test@ke.sip.africastalking.com',
+          ringback_tone: 'http://mymediafile.com/playme.mp3',
+          record: true,
+          max_duration: '5',
+          caller_id: '+254711XXXYYY',
+          sequential: true
+        )
+      end
+
+      it 'can create a voice forward_call to call AT api' do
+        expect(voice.forward_call(entity: entity)).to eq('Voice forward_call')
+      end
     end
 
-    it 'can create a voice record call to AT api' do
-      expect(voice.record(entity: entity)).to eq('Voice record')
+    context 'when doing a voice record api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceRecordEntity.new(
+          prompt: 'Please tell us your name after the beep.',
+          finish_on_key: '#',
+          maxLength: '10',
+          trim_silence: true,
+          play_beep: true
+        )
+      end
+
+      it 'can create a voice record call to AT api' do
+        expect(voice.record(entity: entity)).to eq('Voice record')
+      end
     end
 
-    it 'can create a voice enqueue to call AT api' do
-      expect(voice.enqueue(entity: entity)).to eq('Voice enqueue')
+    context 'when doing a voice terminal_record api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceTerminalRecordEntity.new(
+          prompt: 'Please leave a message after the beep.',
+          play_beep: true
+        )
+      end
+
+      it 'can create a voice terminal_record to call AT api' do
+        expect(voice.terminal_record(entity: entity)).to eq('Voice terminal_record')
+      end
     end
 
-    it 'can create a voice dequeue call to AT api' do
-      expect(voice.dequeue(entity: entity)).to eq('Voice dequeue')
+    context 'when doing a voice enqueue api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceEnqueueEntity.new(
+          queue_name: 'test',
+          hold_music: 'http://www.mymediaserver.com/audio/callWaiting.wav'
+        )
+      end
+
+      it 'can create a voice enqueue to call AT api' do
+        expect(voice.enqueue(entity: entity)).to eq('Voice enqueue')
+      end
     end
 
-    it 'can create a voice redirect to call AT api' do
-      expect(voice.redirect(entity: entity)).to eq('Voice redirect')
+    context 'when doing a voice dequeue api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceDequeueEntity.new(
+          queue_name: 'test',
+          phone_number: '+254711082XXX'
+        )
+      end
+
+      it 'can create a voice dequeue call to AT api' do
+        expect(voice.dequeue(entity: entity)).to eq('Voice dequeue')
+      end
     end
 
-    it 'can create a voice reject call to AT api' do
-      expect(voice.reject).to eq('Voice reject')
+    context 'when doing a voice redirect api call' do
+      let(:entity) do
+        Africas::Talking::Services::VoiceRedirectEntity.new(
+          handler_url: 'http://www.myotherhandler.com/process.php'
+        )
+      end
+
+      it 'can create a voice redirect to call AT api' do
+        expect(voice.redirect(entity: entity)).to eq('Voice redirect')
+      end
+    end
+
+    context 'when doing a voice reject api call' do
+      it 'can create a voice reject call to AT api' do
+        expect(voice.reject).to eq('Voice reject')
+      end
     end
   end
 end
